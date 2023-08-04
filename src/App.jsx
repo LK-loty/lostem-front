30729bd3ef85a781920d1258b3132014bbc0aea7
ljw43 <leejiwon_tech@naver.com>
2023-08-04 21:29:14 +0900

    [Feat] LostItemPostPage route 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LostItemPostPage from "./pages/LostItemPostPage/LostItemPostPage";
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/LostItemPost" element={<LostItemPostPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
