756b4f7d07a56924e8cccedda60252ccf7be506c
ljw43 <leejiwon_tech@naver.com>
2023-08-06 00:10:14 +0900

    [Feat] Navigation 생성

---
+++ src/components/common/Navigation/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ImgLoty from "../../../assets/images/img_loty.png";
+import ImgSprout from "../../../assets/images/img_sprout.png";
+import "./style.scss";
+
+const navItems = [
+  { id: 1, name: "주인을찾아요", link: "/" },
+  { id: 2, name: "잃어버렸어요", link: "/lostitemlist" },
+  { id: 3, name: "글쓰기", link: "/post" },
+  { id: 4, name: "채팅", link: "/chat" },
+  { id: 5, name: "키워드알림", link: "/keyword" },
+  { id: 6, name: "내정보", link: "/myinfo" },
+];
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <img className="logo" src={ImgLoty} alt="loty" />
+      <div className="auth">
+        <Link className="btn-login" to="/login">
+          로그인
+        </Link>
+        <Link className="btn-signup" to={"/signup"}>
+          회원가입
+        </Link>
+      </div>
+      <ul>
+        {navItems.map((nav) => {
+          return (
+            <li key={nav.id}>
+              <Link className="navigation-menu" to={nav.link}>
+                <img className="imgsprout" src={ImgSprout} alt="sprout" />
+                {nav.name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+export default Navigation;
