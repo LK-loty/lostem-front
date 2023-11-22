a9746c4730189270a32dd529f9b60830919ccbb7
ljw43 <leejiwon_tech@naver.com>
2023-11-22 15:43:18 +0900

    [Feat] id/password 찾기 페이지 route 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import HomePage from "./pages/HomePage";
 import PostPage from "./pages/PostPage";
 import LogInPage from "./pages/LogInPage";
 import SignUpPage from "./pages/SignUpPage";
+import FindIdPage from "./pages/FindIdPage";
+import FindPasswordPage from "./pages/FindPasswordPage";
 import NotFound from "./pages/NotFound";
 
 const App = () => {
@@ -12,6 +14,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LogInPage />} />
+        <Route path="findid" element={<FindIdPage />} />
+        <Route path="findpassword" element={<FindPasswordPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/post" element={<PostPage />} />
         <Route path="/*" element={<NotFound />} />
