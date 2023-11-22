ca9434ebc88394e5b8bd71b7d7f77fe616330347
ljw43 <leejiwon_tech@naver.com>
2023-11-22 15:39:15 +0900

    [Refactor] axios 기본 설정에서 withwithCredentials 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import axios from "axios";
 import HomePage from "./pages/HomePage";
 import PostPage from "./pages/PostPage";
 import LogInPage from "./pages/LogInPage";
 import SignUpPage from "./pages/SignUpPage";
 import NotFound from "./pages/NotFound";
-
-axios.defaults.withCredentials = true;
 
 const App = () => {
   return (
