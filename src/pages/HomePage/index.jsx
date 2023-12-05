dbbcaf55b986bfe031e6f0fd09032580e39190a0
ljw43 <leejiwon_tech@naver.com>
2023-12-05 16:53:55 +0900

    [Del] Header 컴포넌트 삭제

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,11 +1,9 @@
 import Navigation from "../../components/common/Navigation";
-import Header from "../../components/common/Header";
 
 const HomePage = () => {
   return (
     <div>
       <Navigation />
-      <Header />
     </div>
   );
 };
--- src/pages/PostPage/index.jsx
+++ src/pages/PostPage/index.jsx
@@ -1,12 +1,10 @@
 import PostForm from "../../components/common/PostForm";
 import Navigation from "../../components/common/Navigation";
-import Header from "../../components/common/Header";
 
 const PostPage = () => {
   return (
     <div className="postpage">
       <Navigation />
-      <Header />
       <PostForm />
     </div>
   );
