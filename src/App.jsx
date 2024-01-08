62eab50916c01ceaf44ce207b70c8da15c615890
ljw43 <leejiwon_tech@naver.com>
2024-01-08 23:18:26 +0900

    [Chore] Route 추가 및 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import axios from "axios";
 import { Routes, Route } from "react-router-dom";
 import FindItemPage from "./pages/FindItemPage";
 import FindOwnerPage from "./pages/FindOwnerPage";
-import PostPage from "./pages/PostPage";
+import FindItemPostPage from "./pages/PostPage/FindItemPostPage";
+import FindOwnerPostPage from "./pages/PostPage/FindOwnerPostPage";
 import LogInPage from "./pages/LogInPage";
 import SignUpPage from "./pages/SignUpPage";
 import FindIdPage from "./pages/FindIdPage";
@@ -11,6 +12,7 @@ import FindPasswordPage from "./pages/FindPasswordPage";
 import ChatPage from "./pages/ChatPage";
 import NotFound from "./pages/NotFound";
 import PrivateRoute from "./components/PrivateRoute";
+import MainLayout from "./components/common/MainLayout";
 import "./styles/main.scss";
 
 axios.defaults.baseURL = "http://localhost:8080/";
@@ -18,18 +20,21 @@ const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<FindItemPage />} />
-        <Route path="/findowner" element={<FindOwnerPage />} />
         <Route path="/login" element={<LogInPage />} />
         <Route path="/findid" element={<FindIdPage />} />
         <Route path="/findpassword" element={<FindPasswordPage />} />
         <Route path="/signup" element={<SignUpPage />} />
-        {/* 로그인 해야 접근 가능한 페이지 */}
-        <Route element={<PrivateRoute />}>
-          <Route path="/post" element={<PostPage />} />
-          <Route path="/chat" element={<ChatPage />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<FindItemPage />} />
+          <Route path="/findowner" element={<FindOwnerPage />} />
+          {/* 로그인 해야 접근 가능한 페이지 */}
+          <Route element={<PrivateRoute />}>
+            <Route path="/post" element={<FindItemPostPage />} />
+            <Route path="/findowner/post" element={<FindOwnerPostPage />} />
+            <Route path="/chat" element={<ChatPage />} />
+          </Route>
         </Route>
-        <Route path="/*" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
