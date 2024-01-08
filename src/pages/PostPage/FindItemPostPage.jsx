17c6b644e06807cc90ab4673acfd9458afbc6c78
ljw43 <leejiwon_tech@naver.com>
2024-01-08 21:12:06 +0900

    [Feat] PostPage를 FindItemPage와 FindOwnerPage로 분리

---
+++ src/pages/PostPage/FindItemPostPage.jsx
@@ -0,0 +1,10 @@
+import Lost from "../../components/PostForm/Lost";
+
+const FindItemPostPage = () => {
+  return (
+    <div className="postpage">
+      <Lost />
+    </div>
+  );
+};
+export default FindItemPostPage;
---
+++ src/pages/PostPage/FindOwnerPostPage.jsx
@@ -0,0 +1,10 @@
+import Found from "../../components/PostForm/Found";
+
+const FindOwnerPostPage = () => {
+  return (
+    <div className="postpage">
+      <Found />
+    </div>
+  );
+};
+export default FindOwnerPostPage;
