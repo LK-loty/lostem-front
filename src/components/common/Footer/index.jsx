e865a8eae68af1e257e6c04d014eea70392cbbb9
ljw43 <leejiwon_tech@naver.com>
2024-01-12 17:35:58 +0900

    [Feat] Footer 컴포넌트 추가

---
+++ src/components/common/Footer/index.jsx
@@ -0,0 +1,13 @@
+import grayLoty from "../../../assets/images/img_gray-loty.png";
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <img src={grayLoty} />
+      </div>
+      COPYRIGHT(c) LOSTEM. ALL RIGHTS RESERVED. <br />
+      e-mail. lostem@naver.com
+    </footer>
+  );
+};
+export default Footer;
