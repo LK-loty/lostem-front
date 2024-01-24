e8de021a02749de7eaebd3c24243623e79e7d472
ljw43 <leejiwon_tech@naver.com>
2024-01-24 09:12:52 +0900

    [Chore] ChatMessage 상대방/본인 구분

--- src/components/Chat/ChatMessage/index.jsx
+++ src/components/Chat/ChatMessage/index.jsx
@@ -1,7 +1,20 @@
-const ChatMessage = React.memo(({ message, isMyMessage }) => {
+import React from "react";
+
+const ChatMessage = React.memo(({ message, isMyMessage, avatar, time }) => {
   return (
     <div className={isMyMessage ? "my-message" : "other-message"}>
-      <p>{message}</p>
+      {isMyMessage ? (
+        <>
+          {time && <span className="message-time">{time}</span>}
+          <p>{message}</p>
+        </>
+      ) : (
+        <>
+          <img src={avatar} alt="Profile" className="profile-image" />
+          <p>{message}</p>
+          {time && <span className="message-time">{time}</span>}
+        </>
+      )}
     </div>
   );
 });
