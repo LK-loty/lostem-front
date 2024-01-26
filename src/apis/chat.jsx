6e8a65b323b67b01946971908b3782de02664c1b
ljw43 <leejiwon_tech@naver.com>
2024-01-26 10:12:10 +0900

    [Feat] 특정 채팅방 조회 api 추가

--- src/apis/chat.jsx
+++ src/apis/chat.jsx
@@ -8,9 +8,28 @@ export const readChatList = async () => {
       Authorization: `Bearer ${accessToken}`,
     };
 
-    const response = await axios.post("api/chat/room/read/chat", {
+    const response = await axios.post("api/chat/room/read/user", {
       headers,
     });
+
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+// 특정 채팅방 조회
+export const readChatRoom = async (roomId) => {
+  try {
+    const accessToken = localStorage.getItem("act");
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+
+    const response = await axios.get(`api/chat/room/read/${roomId}`, {
+      headers,
+    });
+
     return response;
   } catch (error) {
     return error.response;
