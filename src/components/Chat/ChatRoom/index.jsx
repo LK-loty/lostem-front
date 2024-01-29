9ebd9f897ce1f47347b32c6ce7b3338858238e8e
ljw43 <leejiwon_tech@naver.com>
2024-01-30 01:38:49 +0900

    [Feat] 드롭다운메뉴 추가 및 버튼 스타일 변경

--- src/components/Chat/ChatRoom/index.jsx
+++ src/components/Chat/ChatRoom/index.jsx
@@ -8,6 +8,8 @@ const ChatRoom = () => {
   const { register, handleSubmit } = useForm();
   const { roomId } = useParams(); // 채팅방 아이디
 
+  const [isDropOpen, setIsDropOpen] = useState(false);
+
   // roomId가 변경될때마다 해당 채팅방 데이터 가져오기
   useEffect(() => {}, [roomId]);
 
@@ -15,21 +17,34 @@ const ChatRoom = () => {
     <div className="chatroom">
       <div className="header">
         <div className="user-info">
-          <img className="profile_img" />
-          <span>상대닉네임</span>
-        </div>
-        <div className="item-info">
-          <img />
           <div>
-            <span>제목</span>
-            <button>거래완료</button>
-            {/* 후기 작성은 거래 완료 상태일 때만 활성화 */}
-            <button>후기작성</button>
+            <img className="profile_img" />
+            <span>상대닉네임</span>
+          </div>
+          <div className="dropdown">
+            <button onClick={() => setIsDropOpen(!isDropOpen)}>
+              <PiDotsThreeVerticalBold size={24} />
+            </button>
+            {isDropOpen && (
+              <ul className="menu-list">
+                <li>채팅방 나가기</li>
+                <li>신고하기</li>
+              </ul>
+            )}
           </div>
         </div>
-        <button>
-          <PiDotsThreeVerticalBold size={24} />
-        </button>
+        <hr />
+        <div className="item-info">
+          <div>
+            <div>
+              <img />
+              <span>제목</span>
+            </div>
+            <button className="fill-green-button">거래완료</button>
+            {/* 후기 작성은 거래 완료 상태일 때만 활성화 */}
+            <button className="fill-green-button">후기작성</button>
+          </div>
+        </div>
       </div>
       <div className="chat-messages"></div>
       <form className="message-input">
@@ -39,7 +54,7 @@ const ChatRoom = () => {
           maxLength={1000}
         />
         <button type="button">
-          <BsFillSendFill size={15} />
+          <BsFillSendFill size={14} />
         </button>
       </form>
     </div>
