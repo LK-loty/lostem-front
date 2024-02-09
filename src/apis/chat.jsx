1fd1e31bc866f0db156d263fceb98f9810ec8698
ljw43 <leejiwon_tech@naver.com>
2024-02-09 09:38:43 +0900

    [Feat] 채팅방 전체 메시지 조회 api 추가

--- src/apis/chat.jsx
+++ src/apis/chat.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// 채팅 목록
+// 채팅방 목록
 export const readChatList = async () => {
   try {
     const accessToken = localStorage.getItem("act");
@@ -27,6 +27,24 @@ export const readChatRoom = async (roomId) => {
     };
 
     const response = await axios.get(`api/chat/room/read/${roomId}`, {
+      headers,
+    });
+
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+// 채팅방의 전체 메시지 조회
+export const readMessages = async (roomId) => {
+  try {
+    const accessToken = localStorage.getItem("act");
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+
+    const response = await axios.get("api/chat/get", {
       headers,
     });
 
