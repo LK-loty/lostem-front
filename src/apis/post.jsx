c147fbfc62424ec8840d8e013a5a1271c8396fc2
ljw43 <leejiwon_tech@naver.com>
2024-04-16 23:31:35 +0900

    [Feat] post api 함수 추가
    - 글 수정, 삭제, 상태 수정, 리뷰 작성 함수 추가

--- src/apis/post.jsx
+++ src/apis/post.jsx
@@ -8,7 +8,7 @@ export const postLost = async (data) => {
       Authorization: `Bearer ${accessToken}`,
     };
 
-    const response = await axios.post("api/lost/create", data, {
+    const response = await axios.post("/api/lost/create", data, {
       headers,
     });
 
@@ -26,7 +26,7 @@ export const postFound = async (data) => {
       Authorization: `Bearer ${accessToken}`,
     };
 
-    const response = await axios.post("api/found/create", data, {
+    const response = await axios.post("/api/found/create", data, {
       headers,
     });
     return response;
@@ -38,7 +38,7 @@ export const postFound = async (data) => {
 // 글목록 조회 - 잃어버렸어요
 export const readLost = async (page) => {
   try {
-    const response = await axios.get("api/lost/read", {
+    const response = await axios.get("/api/lost/read", {
       params: {
         page: page - 1,
       },
@@ -54,7 +54,7 @@ export const readLost = async (page) => {
 // 글목록 조회 - 주인을찾아요
 export const readFound = async (page) => {
   try {
-    const response = await axios.get("api/found/read", {
+    const response = await axios.get("/api/found/read", {
       params: {
         page: page - 1,
       },
@@ -84,6 +84,115 @@ export const readFoundDetail = async (postId) => {
     return response;
   } catch (error) {
     console.log("readFoundDetail api 에러", error);
+    return error.response;
+  }
+};
+
+// 글 수정 - 주인을찾아요
+export const updateFound = async (data) => {
+  try {
+    const accessToken = localStorage.getItem("act");
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+
+    const response = await axios.patch("/api/found/update", data, { headers });
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+// 글 수정 - 잃어버렸어요
+export const updateLost = async (data) => {
+  try {
+    const accessToken = localStorage.getItem("act");
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+
+    const response = await axios.patch("/api/lost/update", data, { headers });
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+// 글 상태 수정 - 주인을찾아요
+export const updateFoundState = async (data) => {
+  try {
+    const accessToken = localStorage.getItem("act");
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+
+    const response = await axios.patch("/api/found/change", data, { headers });
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+// 글 상태 수정 - 잃어버렸어요
+export const updateLostState = async (data) => {
+  try {
+    const accessToken = localStorage.getItem("act");
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+
+    const response = await axios.patch("/api/lost/change", data, { headers });
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+// 글 삭제
+export const deleteFound = async (postId) => {
+  try {
+    const accessToken = localStorage.getItem("act");
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+
+    const config = {
+      headers: headers,
+    };
+
+    const response = await axios.delete(`/api/found/delete/${postId}`, config);
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+// 글 삭제
+export const deleteLost = async (postId) => {
+  try {
+    const accessToken = localStorage.getItem("act");
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+
+    const config = {
+      headers: headers,
+    };
+
+    const response = await axios.delete(`/api/lost/delete/${postId}`, config);
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+// 평가글 작성
+export const writeReview = async (data, type) => {
+  try {
+    const response = await axios.post(``);
+    return response;
+  } catch (error) {
+    console.log("평가글 작성 api 에러 => ", error);
     return error.response;
   }
 };
